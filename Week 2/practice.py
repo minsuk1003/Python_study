a290efd3bf983fdf75d137b4fa068f3c18c8d85f
minsuk1003 <kms1003go@khu.ac.kr>
2023-09-13 01:32:53 +0900

    9.12

--- Week 2/practice.py
+++ Week 2/practice.py
@@ -1,4 +1,4 @@
-# 리스트
+# 리스트 : 여러 값 저장
 ssg = ["김광현", "최정", "김강민"]
 print(ssg)
 
@@ -18,7 +18,7 @@ ssg.clear()
 print(len(ssg))
 
 
-# 딕셔너리
+# 딕셔너리 - key : value 페어
 ssg_num = {
     "김광현" : 29,
     "최정" : 14,
@@ -40,3 +40,27 @@ print(ssg_num.items())
 
 
 ssg_num.clear()
+
+# 튜플 - 값 수정 불가
+ssg_legend = ("박경완", "김광현", "최정")
+
+# 집합 - 중복 불가, 순서 없음
+python = {"강민석", "김진수", "심건우"}
+sql = {"강민석", "유지수", "강세영", "조윤주"}
+
+print(sql.intersection(python)) # 교집합
+print(sql.union(python)) # 합집합
+print(sql - python) # 차집합
+print(python - sql)
+
+python.add("강세영")
+print(python)
+
+sql.remove("조윤주")
+print(sql)
+
+# 자료형 변경
+menu = {"김밥", "떡볶이", "라면", "잔치국수"}
+menu = list(menu)
+menu = tuple(menu)
+
