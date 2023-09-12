65dce428fc4210c0e9b2c93efecab8d76fe12176
minsuk1003 <kms1003go@khu.ac.kr>
2023-09-13 01:13:26 +0900

    9.12

--- Week 2/practice.py
+++ Week 2/practice.py
@@ -16,3 +16,27 @@ print(ssg)
 
 ssg.clear()
 print(len(ssg))
+
+
+# 딕셔너리
+ssg_num = {
+    "김광현" : 29,
+    "최정" : 14,
+    "김강민" : 0
+}
+
+print(ssg_num["김광현"])
+print(ssg_num.get("추신수")) # key가 없다면 오류 대신 None을 출력
+print(ssg_num.get("최정")) 
+
+print("추신수" in ssg_num) # 없으면 False
+print("김강민" in ssg_num) # 있으면 True
+
+ssg_num["추신수"] = 17
+
+print(ssg_num.keys())
+print(ssg_num.values())
+print(ssg_num.items())
+
+
+ssg_num.clear()
