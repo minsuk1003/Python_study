dd80cda224fc64deb1c4095a380ee3c7313826f6
minsuk1003 <kms1003go@khu.ac.kr>
2023-09-13 00:57:46 +0900

    9.12

--- Week 2/practice.py
+++ Week 2/practice.py
@@ -1,3 +1,18 @@
 # 리스트
+ssg = ["김광현", "최정", "김강민"]
+print(ssg)
 
-ssg = ["김광현", "최정"]
+ssg.append("추신수")
+print(ssg)
+
+ssg.insert(1, "서진용")
+print(ssg)
+
+ssg.pop()
+
+
+ssg.sort()
+print(ssg)
+
+ssg.clear()
+print(len(ssg))
