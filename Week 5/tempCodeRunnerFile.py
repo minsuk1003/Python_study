e41cfe6bd68cb7438c0c9a6436e47fbada33b167
minsuk1003 <kms1003go@khu.ac.kr>
2023-10-10 19:54:28 +0900

    10.10

---
+++ Week 5/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# enumerate
+for i, model in enumerate(["SVM", "DT", "LR", "XGBoost"]):
+    print(i, model)
+
+# eval
+eval("1,2")
--- Week 5/try.py
+++ Week 5/try.py
@@ -1,8 +1,10 @@
+# 예외 처리
 try:
     4/0
 except ZeroDivisionError as e:
     print(e)
 
+# 예외 만들기
 class MyError(Exception):
     def __str__(self):
         return "허용되지 않는 별명입니다."
